9216bc9750bfe35db0afedff158e507680fbe560
sminy85 <smini857@naver.com>
2021-02-24 14:30:22 +0900

    edit font size

--- src/templates/blog-list.tsx
+++ src/templates/blog-list.tsx
@@ -59,7 +59,7 @@ const BlogIndex = ({
               <h3
                 style={{
                   marginBottom: rhythm(1 / 4),
-                  fontSize: '2.5rem',
+                  fontSize: '2rem',
                 }}
               >
                 <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
